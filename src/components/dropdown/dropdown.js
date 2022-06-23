72d6a6afcc8cae65bf5b6ae5e9206eef70ad0784
Lapin Andrey <nethack@mail.ru>
2022-06-23 21:03:48 +0400

    Added dropdown component, add new prop to buttoncomponent

---
+++ src/components/dropdown/dropdown.js
@@ -0,0 +1,93 @@
+import {
+  counter,
+  decrementCounter,
+  incrementCounter,
+  clearState,
+} from './dropdownCounterState';
+
+const dropdownElements = document.querySelectorAll('.dropdown__main');
+const dropdownList = document.querySelectorAll('.dropdown__list');
+const buttons = document.querySelectorAll(`button[data-button]`);
+
+const allInputs = [...dropdownElements];
+const allList = [...dropdownList];
+const allButtons = [...buttons];
+
+const clearCount = (e) => {
+  const dataset = e.target.dataset.button;
+  clearState(dataset);
+  const clearButton = document.querySelector(`button[data-button=${dataset}]`);
+  clearButton.classList.add('button__none');
+  const counterNumbers = document.querySelectorAll(`[data-dropdowncounter]`);
+  [...counterNumbers].forEach((el) => {
+    if (el.dataset.dropdowncounter.includes(dataset)) {
+      // eslint-disable-next-line no-param-reassign
+      el.textContent = 0;
+    }
+  });
+  const allIncDecButtons = document.querySelectorAll(`[data-dropdownbutton]`);
+  [...allIncDecButtons].forEach((el) => {
+    if (el.dataset.dropdownbutton.includes(dataset) && el.textContent === '-') {
+      el.classList.add('dropdown__circle--none');
+    }
+  });
+};
+
+allButtons.forEach((el) => {
+  if (el.textContent === 'применить') {
+    const dataset = el.dataset.button;
+    const target = document.querySelector(`[data-dropdown=${dataset}]`);
+    el.addEventListener('click', () => {
+      target.nextElementSibling.classList.add('dropdown__bottomField--none');
+    });
+  } else if (el.textContent === 'очистить') {
+    el.addEventListener('click', (e) => {
+      clearCount(e);
+    });
+  }
+});
+
+const changeVisibility = (e) => {
+  const datasetParent = e.target.parentNode.dataset.dropdown;
+  if (!datasetParent) return;
+  const target = allInputs.find((el) => el.dataset.dropdown === datasetParent);
+  target.firstChild.classList.toggle('dropdown__field--active');
+  target.nextElementSibling.classList.toggle('dropdown__bottomField--none');
+};
+
+allInputs.forEach((el) => el.addEventListener('click', (e) => changeVisibility(e)));
+
+const counterHandler = (e) => {
+  const dataset = e.target.dataset.dropdownbutton;
+
+  if (!dataset) return;
+  const uniqDropDownId = dataset.split('-')[0];
+  const clearButton = document.querySelector(`button[data-button=${uniqDropDownId}]`);
+
+  const whatIsButton = e.target.textContent;
+
+  const datasetCounter = document.querySelector(`[data-dropdowncounter='${dataset}']`);
+
+  if (whatIsButton === '+') {
+    incrementCounter(dataset);
+    datasetCounter.textContent = counter[dataset];
+    clearButton.classList.remove('button__none');
+  } else {
+    decrementCounter(dataset);
+    datasetCounter.textContent = counter[dataset];
+  }
+  console.log(counter);
+  if (counter[uniqDropDownId] === 0) {
+    clearButton.classList.add('button__none');
+  }
+
+  const incremButton = document.getElementById(dataset);
+
+  if (counter[dataset] > 0) {
+    incremButton.classList.remove('dropdown__circle--none');
+  } else if (counter[dataset] === 0) {
+    incremButton.classList.add('dropdown__circle--none');
+  }
+};
+
+allList.forEach((el) => el.addEventListener('click', (e) => counterHandler(e)));
---
+++ src/components/dropdown/dropdownCounterState.js
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-mutable-exports */
+const allCountersQuantity = document.querySelectorAll('[data-dropdowncounter]');
+
+export let counter = {};
+
+const initCounter = () => {
+  [...allCountersQuantity].forEach((el) => {
+    const nameTarget = el.dataset.dropdowncounter;
+    const quantityTarget = +el.textContent;
+    counter = {
+      ...counter,
+      [nameTarget]: quantityTarget,
+      [nameTarget.split('-')[0]]: 0,
+    };
+  });
+};
+
+export const incrementCounter = (name) => {
+  counter = {
+    ...counter,
+    [name]: counter[name] + 1,
+    [name.split('-')[0]]: counter[name.split('-')[0]] + 1,
+  };
+};
+
+export const decrementCounter = (name) => {
+  if (counter[name] <= 0) return;
+  counter = {
+    ...counter,
+    [name]: counter[name] - 1,
+    [name.split('-')[0]]: counter[name.split('-')[0]] - 1,
+  };
+};
+
+export const clearState = (name) => {
+  console.log('очстка стейта', 'пришел', name, 'сам стейте', counter);
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key in counter) {
+    if (key.includes(name)) {
+      counter[key] = 0;
+    }
+  }
+  console.log('очищенный стейт', 'пришел', name, 'сам стейте', counter);
+};
+
+initCounter();
