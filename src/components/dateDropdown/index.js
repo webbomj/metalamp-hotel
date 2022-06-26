5861b69c8f12a1106a437d60d28443928cd5fc9b
Lapin Andrey <nethack@mail.ru>
2022-06-26 18:05:05 +0400

    Added dateDropdown, update textField (added mask to field)

---
+++ src/components/dateDropdown/index.js
@@ -0,0 +1,35 @@
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
+import { airDatePickerOptionsCreator, dateConversion } from '../airDatePicker/index';
+
+const dateDropdownLeftInput = document.querySelector('.dateDropdownLeft__input');
+const dateDropdownRightInput = document.querySelector('.dateDropdownRight__input');
+const dateDropdownElement = document.querySelector('.dateDropdown__close');
+
+const openCloseDropdownFunction = () => {
+  dateDropdownElement.classList.toggle('dateDropdown__open');
+};
+
+// eslint-disable-next-line no-return-assign
+const changeTitleLeft = (dateString = 'ДД.ММ.ГГГГ') => (dateDropdownLeftInput.value = dateString);
+// eslint-disable-next-line no-return-assign
+const changeTitleRight = (dateString = 'ДД.ММ.ГГГГ') => (dateDropdownRightInput.value = dateString);
+
+const selectDate = (formDate) => {
+  if (formDate.length === 0) {
+    changeTitleLeft();
+    changeTitleRight();
+  } else if (formDate.length === 2) {
+    changeTitleLeft(dateConversion(formDate).from);
+    changeTitleRight(dateConversion(formDate).to);
+  }
+};
+
+dateDropdownLeftInput.parentNode.addEventListener('click', () => openCloseDropdownFunction());
+dateDropdownRightInput.parentNode.addEventListener('click', () => openCloseDropdownFunction());
+
+// eslint-disable-next-line no-unused-vars
+const airDatePickerInst = new AirDatepicker(
+  dateDropdownElement,
+  airDatePickerOptionsCreator(selectDate, openCloseDropdownFunction)
+);
--- src/components/textField/index.js
+++ src/components/textField/index.js
@@ -1,1 +1,17 @@
-console.log('textField open');
+import Inputmask from 'inputmask';
+
+const IMaskSelector = document.querySelectorAll('[date-masked="masked"]');
+
+// Inputmask({ mask: '30.12.2060', maskPlaceholder: 'ДД.ММ.ГГГГ', value: '12' }).mask(IMaskSelector);
+// Inputmask({
+//   // mask: '^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d$',
+//   regex: '^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d$',
+// }).mask(IMaskSelector);
+
+[...IMaskSelector].forEach((el) => {
+  new Inputmask({
+    alias: 'datetime',
+    placeholder: 'ДД.ММ.ГГГГ',
+    inputFormat: 'dd.mm.yyyy',
+  }).mask(el);
+});
