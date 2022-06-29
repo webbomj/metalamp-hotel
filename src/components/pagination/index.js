12962a743704e345705c064e2a188b37e39232aa
Andrey Lapin <nethack@mail.ru>
2022-06-29 17:01:08 +0400

    Added bottom text to pagination component

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -12,14 +12,19 @@ require('paginationjs');
 //   return html;
 // }
 
+const defineNavigatorText = (currentPage) =>
+  `${currentPage * 12 - 11} - ${currentPage * 12} из 100+ вариантов аренды`;
+
 jQuery('#pagination').pagination({
-  dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+  dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17],
   pageSize: 1,
   pageRange: 1,
   autoHidePrevious: true,
   className: 'paginationjs-theme-blue',
   nextText: '',
   prevText: '',
+  showNavigator: true,
+  formatNavigator: defineNavigatorText,
   // callback(data) {
   //   const html = template(data);
 
