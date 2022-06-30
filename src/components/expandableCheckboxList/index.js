53194226d96c7c490cb78129136a5465736a744e
Andrey Lapin <nethack@mail.ru>
2022-06-30 23:28:29 +0400

    Added to expendable checkbox list open  option

--- src/components/expandableCheckboxList/index.js
+++ src/components/expandableCheckboxList/index.js
@@ -1,17 +1,17 @@
-const coll = document.getElementsByClassName('expandableCheckboxes__header');
+const coll = document.querySelectorAll('.expandableCheckboxes__header');
 
 [...coll].forEach((el) => {
   el.addEventListener('click', () => {
     el.classList.toggle('active');
     const content = el.nextElementSibling;
 
-    el.lastChild.classList.toggle('expandableCheckboxes__arrow--rotate');
-
     if (content.style.display === 'block') {
       content.style.display = 'none';
     } else {
       content.style.display = 'block';
     }
+
+    el.lastChild.classList.toggle('expandableCheckboxes__arrow--rotate');
 
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
