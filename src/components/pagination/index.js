b745c170e1142aab7d37b961ecd7a5fe1bd477cf
Andrey Lapin <nethack@mail.ru>
2022-07-02 18:18:54 +0400

    Added headers: full and small. Finished headersAndFooters page

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -3,6 +3,8 @@ import jQuery from 'jquery';
 window.jQuery = jQuery;
 require('paginationjs');
 
+const pagination = document.querySelector('#pagination');
+
 // function template(data) {
 //   let html = '<ul>';
 //   jQuery.each(data, (index, item) => {
@@ -15,19 +17,21 @@ require('paginationjs');
 const defineNavigatorText = (currentPage) =>
   `${currentPage * 12 - 11} - ${currentPage * 12} из 100+ вариантов аренды`;
 
-jQuery('#pagination').pagination({
-  dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17],
-  pageSize: 1,
-  pageRange: 1,
-  autoHidePrevious: true,
-  className: 'paginationjs-theme-blue',
-  nextText: '',
-  prevText: '',
-  showNavigator: true,
-  formatNavigator: defineNavigatorText,
-  // callback(data) {
-  //   const html = template(data);
+if (pagination) {
+  jQuery('#pagination').pagination({
+    dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17],
+    pageSize: 1,
+    pageRange: 1,
+    autoHidePrevious: true,
+    className: 'paginationjs-theme-blue',
+    nextText: '',
+    prevText: '',
+    showNavigator: true,
+    formatNavigator: defineNavigatorText,
+    // callback(data) {
+    //   const html = template(data);
 
-  //   jQuery('#data-container').html(html);
-  // },
-});
+    //   jQuery('#data-container').html(html);
+    // },
+  });
+}
