4e92fa40f47828d844aa5ebbf48d8de619cf2d47
Andrey Lapin <nethack@mail.ru>
2022-07-03 15:31:40 +0400

    Added findRooms component

--- src/components/room/index.js
+++ src/components/room/index.js
@@ -4,6 +4,7 @@ import Swiper, { Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
+// eslint-disable-next-line no-unused-vars
 const swiper = new Swiper('.swiper', {
   modules: [Pagination],
   // direction: 'vertical',
