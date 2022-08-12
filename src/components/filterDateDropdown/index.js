b02407326a758ce4a35d49af91943e0a4cb25497
Andrey Lapin <nethack@mail.ru>
2022-08-12 19:45:18 +0400

    Fixed airDatePicke and filterDateDropdown: then was 2 instance work only 1

--- src/components/filterDateDropdown/index.js
+++ src/components/filterDateDropdown/index.js
@@ -2,28 +2,45 @@ import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 import { airDatePickerOptionsCreator, dateConversion } from '../airDatePicker/index';
 
-const mainBlock = document.querySelector('.filterDateDropdown__main');
-const airDatePickerBlock = document.querySelector('.filterDateDropdown__close');
-const airDatePickerInput = document.querySelector('.filterDateDropdown__main .textField__input');
+const mainBlock = document.querySelectorAll('.filterDateDropdown__main');
+const airDatePickerBlocks = document.querySelectorAll('.filterDateDropdown__close');
+// const airDatePickerInput = document.querySelector('.filterDateDropdown__main .textField__input');
 
-// eslint-disable-next-line no-return-assign
-const changeTitle = (dateString = '') => (airDatePickerInput.value = dateString);
+// eslint-disable-next-line no-return-assign, no-param-reassign
+const changeTitle = (el, dateString = '') => (el.value = dateString);
 
-const closeAirDatePickerToogle = () =>
-  airDatePickerBlock.classList.toggle('filterDateDropdown__dateOpen');
+const toggleAirDatePicker = (el) => {
+  const airDatePickerNode = el.parentNode.parentNode.parentNode.querySelector(
+    '.filterDateDropdown__close'
+  );
+  airDatePickerNode.classList.toggle('filterDateDropdown__dateOpen');
+};
 
-const selectDate = (formDate) => {
+const closeAirDatePickerToggle = ({ $datepicker }) => {
+  const datePickerNode = $datepicker.parentNode;
+
+  datePickerNode.classList.toggle('filterDateDropdown__dateOpen');
+};
+
+const selectDate = (formDate, datePicker) => {
+  const datePickerNode = datePicker.$datepicker.parentNode;
+  const currentInput = datePickerNode.parentNode.querySelector(
+    '.filterDateDropdown__main .textField__input'
+  );
+
   if (formDate.length === 0) {
-    changeTitle();
+    changeTitle(currentInput);
   } else if (formDate.length === 2) {
-    changeTitle(dateConversion(formDate).resultDate);
+    changeTitle(currentInput, dateConversion(formDate).resultDate);
   }
 };
 
-mainBlock?.addEventListener('click', () => closeAirDatePickerToogle());
+[...mainBlock].forEach((el) => el.addEventListener('click', () => toggleAirDatePicker(el)));
 
-// eslint-disable-next-line no-unused-vars
-const airDatePickerInst = new AirDatepicker(
-  airDatePickerBlock,
-  airDatePickerOptionsCreator(selectDate, closeAirDatePickerToogle)
-);
+[...airDatePickerBlocks].forEach((el) => {
+  // eslint-disable-next-line no-unused-vars
+  const airDatePickerInst = new AirDatepicker(
+    el,
+    airDatePickerOptionsCreator(selectDate, closeAirDatePickerToggle)
+  );
+});
